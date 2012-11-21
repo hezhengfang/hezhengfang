093b619bc4c371f0679953701a439a85c7a9e275
hezhengfang <zhengfang.he@gragonflow.com>
2012-11-21 09:25:19 +0800

    day03

---
+++ Car.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+@interface Car:NSObject
+{
+ int year;
+ NSString *make;
+ NSString *model;
+ 
+}
+-  (void) setMake : (NSString *) aMake andModel : (NSString*) aModel
+andYear : (int)  ayear;
+- (void) printCarInfo;
+- (int) year;
+@end
